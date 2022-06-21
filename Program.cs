a95343dd92fec8de647035bb7e3bab50c93468d8
Gerardo Seoane <gerardo.seoane@finning.com>
2022-06-21 16:48:18 -0500

    Upload Code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validar;
+
+namespace CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validaciones Val = new Validaciones();
+
+            string Respuesta = Val.Validar_conexion();
+            Console.WriteLine(Respuesta);
+
+            String Respuesta1 = Val.Validar_Desconexion();
+            Console.WriteLine(Respuesta1);
+
+            Console.ReadKey();
+        }
+    }
+}
